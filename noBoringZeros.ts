d7d0169f49d48f4c703df074885a345d11b8911a
Ahmed Yasser <ahmedyasser3033@gmail.com>
2024-06-01 02:17:41 +0200

    documented

--- noBoringZeros.ts
+++ noBoringZeros.ts
@@ -1,3 +1,35 @@
+
+// https://www.codewars.com/kata/570a6a46455d08ff8d001002/solutions/typescript
+
+
+/**
+ * Removes trailing zeros from a given number.
+ *
+ * @param {number} n - The input number from which trailing zeros should be removed.
+ * @returns {number} - The number with trailing zeros removed.
+ *
+ * @example
+ * // returns 145
+ * noBoringZeros(1450);
+ *
+ * @example
+ * // returns 96
+ * noBoringZeros(960000);
+ *
+ * @example
+ * // returns 105
+ * noBoringZeros(1050);
+ *
+ * @example
+ * // returns -105
+ * noBoringZeros(-1050);
+ *
+ * @example
+ * // returns 0
+ * noBoringZeros(0);
+ */
+
+
 export function noBoringZeros(n: number): number {
     let numberStr = n.toString();
     
